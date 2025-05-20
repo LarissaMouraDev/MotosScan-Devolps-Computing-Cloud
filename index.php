6ba2a2bc51d0abbe7c8f059bd136b175eaa8186c
Larissa Moura <larissamourafacebook@gmail.com>
2025-05-20 07:48:22 -0300

    Delete index.php

--- index.php
+++
@@ -1,7 +0,0 @@
- <?php
- $mysqli = new mysqli("saes-sql", "viniuser", "vini123", "saes-db");
- if ($mysqli->connect_error) {
- die("Erro na conexão: " . $mysqli->connect_error);
- }
- echo "Conectado com sucesso ao Banco de Dados  saes-db!";
- ?>
